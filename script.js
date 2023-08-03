1119e6dd3cd4a591d2c37a8ebea390bfcd9aa2a4
muhammadhussainharoon <hussain356q@gmail.com>
2023-08-03 13:36:51 +0500

    js practice

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-var islamiat = 45
-var math = 95
-var urdu = 88
-var sindhi = 65
-var totalmarks = 350
-var obtainmraks = (islamiat+math+urdu+sindhi)
-var percentage = (obtainmraks/totalmarks*100)
-console.log(percentage)
+
+
+
+var feroz =prompt ("enter your name")
+var math =+prompt ("enter your math marks")
+var physics =+prompt ("enter your physics marks")
+var english =+prompt ("enter your english marks")
+var urdu  =+prompt ("enter your urdu marks")
+var totalmarks = (400)
+var obtainmarks = (math+physics+english+urdu)
+var percentage = (obtainmarks/totalmarks*100)
+console.log(feroz,math,physics,english,urdu,percentage)
+console.log(feroz + "total marks is" + obtainmarks + "out of" + totalmarks + "and percentage is"+ percentage +"%")
+document.write(feroz, "total marks is" + obtainmarks + "out of" + totalmarks + "and percentage is"+ percentage +"%")
+
+
+
