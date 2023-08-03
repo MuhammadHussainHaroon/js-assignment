b8db57ffd24dd468273094ce53fe9e3ff39a86e6
muhammadhussainharoon <hussain356q@gmail.com>
2023-08-03 11:30:36 +0500

    js practice

---
+++ script.js
@@ -0,0 +1,8 @@
+var islamiat = 45
+var math = 95
+var urdu = 88
+var sindhi = 65
+var totalmarks = 350
+var obtainmraks = (islamiat+math+urdu+sindhi)
+var percentage = (obtainmraks/totalmarks*100)
+console.log(percentage)
